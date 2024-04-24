9f55b374bf97eeefd7e635cd34e16a406e01a5c3
bbdk612 <sircenkom239@gmail.com>
2024-04-25 02:26:54 +0500

    start for creation manager client

--- FoodTravelApp/app/app.ts
+++ FoodTravelApp/app/app.ts
@@ -4,9 +4,12 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { Application } from '@nativescript/core'
+import { Application } from "@nativescript/core";
+import { firebase, FirebaseOptions } from "@nativescript/firebase-core";
 
-Application.run({ moduleName: 'app-root' })
+const defaultApp = await firebase().initializeApp();
+
+Application.run({ moduleName: "app-root" });
 
 /*
 Do not place any code after the application has been started as it will not
