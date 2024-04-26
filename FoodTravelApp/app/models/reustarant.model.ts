989feb57320f32805690348ed3b68d8620452eb7
Kidcreole2 <131256373+Kidcreole2@users.noreply.github.com>
2024-04-26 07:54:11 +0500

    Reustarant model fix

--- FoodTravelApp/app/models/reustarant.model.ts
+++ FoodTravelApp/app/models/reustarant.model.ts
@@ -2,5 +2,7 @@ export interface ReustarantModel {
     id: number,
     title: string,
     photo: string,
-    description: string
+    description: string,
+    adress: string,
+    phoneNum: string
 }
